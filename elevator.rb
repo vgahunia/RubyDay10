de4eea1c6a1050879157a3cde337e4509c03e042
Vivek Gahunia <vgahunia@gmail.com>
2015-09-03 13:17:53 -0400

    second commit

--- elevator.rb
+++ elevator.rb
@@ -6,18 +6,23 @@ class Elevator
   def greet
   	puts "Good Morning!!!!"
   end
-  def up
+  def up_down
   	puts "Do you want to go up or down?"
     answer = gets
-
-    puts "I will take you:"
-    puts answer.upcase
+    puts "What floor would you like to go to?"
+    up = gets.to_i
+    if up == floor
+      puts ("We're already here.")
+    elsif up > floor
+      where = floor + up
+      puts "We have gone up #{where} floors"
+    else
+      where = floor - up
+      puts "We have gone down #{where} floors"
+    end
   end
 end
 
 one = Elevator.new(12)
 two = Elevator.new(19)
-puts one.up
-puts one.floor
-puts two.floor
-puts two.greet
+one.up_down
